de1a5abe6d06a429d6fdefa52fdd38fe597499f0
KoreTsuki <466712623@qq.com>
2026-01-26 17:18:21 +0800

    实现登录、文字识别部分，跑通项目流程

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,65 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import React from 'react';
+
+const Footer: React.FC = () => {
+  const defaultMessage = '是明月吧 NullPointerException';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: '未来社畜养成基地',
+          title: 'KoreTsuki',
+          href: 'https://github.com/KoreTsuki',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/KoreTsuki',
+          blankTarget: true,
+        },
+        // {
+        //   key: 'AI-BI',
+        //   title: 'AI-BI',
+        //   href: 'https://ant.design',
+        //   blankTarget: true,
+        // },
+      ]}
+    />
+  );
+  // return (
+  //   <DefaultFooter
+  //     style={{
+  //       background: 'none',
+  //     }}
+  //     links={[
+  //       {
+  //         key: 'Ant Design Pro',
+  //         title: 'Ant Design Pro',
+  //         href: 'https://pro.ant.design',
+  //         blankTarget: true,
+  //       },
+  //       {
+  //         key: 'github',
+  //         title: <GithubOutlined />,
+  //         href: 'https://github.com/ant-design/ant-design-pro',
+  //         blankTarget: true,
+  //       },
+  //       {
+  //         key: 'Ant Design',
+  //         title: 'Ant Design',
+  //         href: 'https://ant.design',
+  //         blankTarget: true,
+  //       },
+  //     ]}
+  //   />
+  // );
+};
+
+export default Footer;
